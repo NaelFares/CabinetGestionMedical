0b3ce29faf65f08d876beec34083b0ce7a9707c2
NaelFares <frs.nael@gmail.com>
2023-12-15 10:52:02 +0100

    sdfsd

--- ajout_patient.php
+++ ajout_patient.php
@@ -95,77 +95,79 @@
 </div>
 
 
+<!--Serveur-->
+<?php
+    print_r($_POST);
 
-        <!--Serveur-->
-        <?php
-            print_r($_POST);
+    require('bd_connexion.php');
 
-            require('bd_connexion.php');
+    // Préparation de la requête de test de présence d'un contact
+    $reqExisteDeja = $linkpdo->prepare('SELECT COUNT(*) FROM patient WHERE nom = :nom AND prenom = :prenom');
 
-            // Préparation de la requête de test de présence d'un contact
-            $reqExisteDeja = $linkpdo->prepare('SELECT COUNT(*) FROM patient WHERE nom = :nom AND prenom = :prenom');
+    //Test de la requete de présence d'un contact => die si erreur
+    if($reqExisteDeja == false) {
+        die("Erreur de préparation de la requête de test de présence d'un patient.");
+    } else {
 
-            //Test de la requete de présence d'un contact => die si erreur
-            if($reqExisteDeja == false) {
-                die("Erreur de préparation de la requête de test de présence d'un patient.");
+        // Liaison des paramètres
+        //PDO::PARAM_STR : C'est le type de données que vous spécifiez pour le paramètre. 
+        //Ici, on indique que :nom doit être traité comme une chaîne de caractères (string). 
+        //Cela permet à PDO de s'assurer que la valeur est correctement échappée et protégée contre les injections SQL
+        $reqExisteDeja->bindParam(':nom', $_POST['nom'], PDO::PARAM_STR);
+        $reqExisteDeja->bindParam(':prenom', $_POST['prenom'], PDO::PARAM_STR);
+
+        // Exécution de la requête
+        $reqExisteDeja->execute();
+
+        //Vérification de la bonne exécution de la requete ExisteDéja
+        //Si oui on arrete et on affiche une erreur
+        //Si non on execute la requete
+        if($reqExisteDeja == false) {
+            die("Erreur dans l'exécution de la requête de test de présence d'un patient.");
+        } else {
+
+            // Récupération du résultat
+            $nbPatients = $reqExisteDeja->fetchColumn();
+
+            // Vérification si le patient existe déjà
+            if ($nbContacts > 0) {
+                echo "Ce patient existe déjà dans la base de données.";
             } else {
+                // Préparation de la requête d'insertion
+                $req = $linkpdo->prepare('INSERT INTO patient(nom, prenom, adresse, ville, cp, date_naissance, lieu_naissance, num_secu_sociale) VALUES(:nom, :prenom, :adresse, :ville, :cp, :date_naissance, :lieu_naissance, :num_secu_sociale)');
 
-                // Liaison des paramètres
-                //PDO::PARAM_STR : C'est le type de données que vous spécifiez pour le paramètre. 
-                //Ici, on indique que :nom doit être traité comme une chaîne de caractères (string). 
-                //Cela permet à PDO de s'assurer que la valeur est correctement échappée et protégée contre les injections SQL
-                $reqExisteDeja->bindParam(':nom', $_POST['nom'], PDO::PARAM_STR);
-                $reqExisteDeja->bindParam(':prenom', $_POST['prenom'], PDO::PARAM_STR);
-
-                // Exécution de la requête
-                $reqExisteDeja->execute();
-
-                //Vérification de la bonne exécution de la requete ExisteDéja
-                //Si oui on arrete et on affiche une erreur
-                //Si non on execute la requete
-                if($reqExisteDeja == false) {
-                    die("Erreur dans l'exécution de la requête de test de présence d'un patient.");
+                // Vérification du fonctionnement de la requete d'insertion
+                if($req == false) {
+                    die('Probleme de la préparation de la requete d\'insertion');
                 } else {
+                    echo ('Ok');
+                }
 
-                    // Récupération du résultat
-                    $nbPatients = $reqExisteDeja->fetchColumn();
+                // Exécution de la requête d'insertion
+                $req->execute(array(
+                    'nom' => $_POST['nom'],
+                    'prenom' => $_POST['prenom'],
+                    'adresse' => $_POST['adresse'],
+                    'ville' => $_POST['ville'],
+                    'cp' => $_POST['cp'],
+                    'date_naissance' => $_POST['date_naissance'],
+                    'lieu_naissance' => $_POST['lieu_naissance'],
+                    'num_secu_sociale' => $_POST['num_secu_sociale'],
+                ));
+                    //Permet de voir comment les requetes SQL agisse sur phpMyAdmin
+                    $req->debugDumpParams();
 
-                    // Vérification si le patient existe déjà
-                    if ($nbContacts > 0) {
-                        echo "Ce patient existe déjà dans la base de données.";
-                    } else {
-                        // Préparation de la requête d'insertion
-                        $req = $linkpdo->prepare('INSERT INTO patient(nom, prenom, adresse, ville, cp, date_naissance, lieu_naissance, num_secu_sociale) VALUES(:nom, :prenom, :adresse, :ville, :cp, :date_naissance, :lieu_naissance, :num_secu_sociale)');
-
-                        // Vérification du fonctionnement de la requete d'insertion
-                        if($req == false) {
-                            die('Probleme de la préparation de la requete d\'insertion');
-                        } else {
-                            echo ('Ok');
-                        }
-
-                        // Exécution de la requête d'insertion
-                        $req->execute(array(
-                            'nom' => $_POST['nom'],
-                            'prenom' => $_POST['prenom'],
-                            'adresse' => $_POST['adresse'],
-                            'ville' => $_POST['ville'],
-                            'cp' => $_POST['cp'],
-                            'date_naissance' => $_POST['date_naissance'],
-                            'lieu_naissance' => $_POST['lieu_naissance'],
-                            'num_secu_sociale' => $_POST['num_secu_sociale'],
-                        ));
-                            //Permet de voir comment les requetes SQL agisse sur phpMyAdmin
-                            $req->debugDumpParams();
-
-                            echo "Le patient a été ajouté avec succès.";
-                    }
-                }   
-            }       
-        ?>
+                    echo "Le patient a été ajouté avec succès.";
+            }
+        }   
+    }       
+?>
     </body>
 
-    <script>
+</html>
+
+
+<script>
         $(document).ready(function(){
 
 //For Card Number formatted input
@@ -223,4 +225,3 @@ expDate.onkeyup = function (e) {
 	});
 })
 </script>
-</html>
