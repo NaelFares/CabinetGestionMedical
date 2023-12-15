ea40783dd1550f180858b2903a7a1619d38934e2
NaelFares <frs.nael@gmail.com>
2023-12-15 08:57:59 +0100

    page connexion

--- index.php
+++ index.php
@@ -5,9 +5,47 @@
         <title>Connexion</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-        <style>
-            body {
-    background: #2c75ff;
+    </head>
+    <body>
+
+<div class="titre">
+    <span>MedicoGest</span>
+</div>
+
+<div class="container mt-5">
+    <div class="row d-flex justify-content-center justify-align-content-center ">
+        <div class="col-md-6">
+        <div class="container-fluid d-flex align-items-center justify-content-center h-100" id="form1">
+            <div class="card px-5 py-5 ">
+                <div class="form-data" v-if="!submitted">
+                    <div class="forms-inputs mb-4"> 
+                        <span>Nom d'utilisateur</span> 
+                    <input autocomplete="off" type="text" v-model="email" v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(email) && emailBlured}" v-on:blur="emailBlured = true">
+                        <div class="invalid-feedback">A valid user is required!</div>
+                    </div>
+                    <div class="forms-inputs mb-4"> 
+                        <span>Mot de passe</span> 
+                    <input autocomplete="off" type="password" v-model="password" v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlured}" v-on:blur="passwordBlured = true">
+                        <div class="invalid-feedback">Password must be 8 character!</div>
+                    </div>
+                    <div class="mb-3"> <button v-on:click.stop.prevent="submit" class="btn btn-dark w-100">Connexion</button> </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
+
+<style>
+
+body {
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(44,117,255,1) 0%, rgba(55,218,252,1) 100%);
+    display : flex;
+    flex-direction : column;
+    align-items: center;
 }
 
 .card {
@@ -18,8 +56,7 @@
     max-width: 2000px; /* Définir une largeur maximale pour la carte */
     margin: 0 auto; /* Centrer la carte */
     justify-content: center;
-    margin-top: 9rem;
-
+    margin-top: 1rem;
 }
 
 .forms-inputs {
@@ -61,35 +98,12 @@
 .bxs-badge-check {
     font-size: 90px;
 }
+
+.titre {
+    margin-top: 4rem;
+    color: white;
+    font-size: 5rem;
+    font-family: 'Lato', sans-serif;
+}
+
 </style>
-    </head>
-    <body>
-
-<div class="container mt-5">
-    <div class="row d-flex justify-content-center justify-align-content-center ">
-        <div class="col-md-6">
-        <div class="container-fluid d-flex align-items-center justify-content-center h-100" id="form1">
-            <div class="card px-5 py-5 ">
-                <div class="form-data" v-if="!submitted">
-                    <div class="forms-inputs mb-4"> 
-                        <span>Nom d'utilisateur</span> 
-                    <input autocomplete="off" type="text" v-model="email" v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(email) && emailBlured}" v-on:blur="emailBlured = true">
-                        <div class="invalid-feedback">A valid user is required!</div>
-                    </div>
-                    <div class="forms-inputs mb-4"> 
-                        <span>Mot de passe</span> 
-                    <input autocomplete="off" type="password" v-model="password" v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlured}" v-on:blur="passwordBlured = true">
-                        <div class="invalid-feedback">Password must be 8 character!</div>
-                    </div>
-                    <div class="mb-3"> <button v-on:click.stop.prevent="submit" class="btn btn-dark w-100">Connexion</button> </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<style
-</body>
-
-
-
-</html>
