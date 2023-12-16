ed462b2ba80a184e5e59229f366180b0e9a159a6
nesriiinee <nesrineben.317@gmail.com>
2023-12-16 22:58:01 +0100

    affichage
    les tableaux, changer le scss car c'est moche comme ca

--- affichage_medecin.php
+++ affichage_medecin.php
@@ -3,16 +3,31 @@
 <head>
     <meta charset="utf-8" />
     <title>Les Medecins</title>
-    <link href="style.css" rel="stylesheet">
+    <link href="style.scss" rel="stylesheet">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <?php
-        require('header.php');
-        require('bd_connexion.php');
+     require('header.php');
+     require('bd_connexion.php');
+     ?>
 
+    <!-- modifier le css du tableau dans style.scss -->
+    <div class="container">
+    <table class="responsive-table">
+        <thead>
+        <tr>
+            <th>Civilité</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+        </tr>
+        </thead>
+        <tbody>
+
+    <?php
+       
         // Préparation de la requête de recherche des patients
         $reqAffichage = $linkpdo->prepare('SELECT civilite, nom, prenom FROM medecin');
 
@@ -25,34 +40,24 @@
             if ($reqAffichage == false) {
                 echo "Erreur dans l'exécution de la requête d'affichage.";
             } else {
-                // Affichage des résultats dans un tableau
-    ?>
-                <!-- faire le css du tableau -->
-                <table border="1">
-                    <tr>
-                        <th>Civilité</th>
-                        <th>Nom</th>
-                        <th>Prénom</th>
-                    </tr>
-    <?php
                 // Récupération des résultats et affichage dans le tableau
                 while ($medecin = $reqAffichage->fetch(PDO::FETCH_ASSOC)) {
                     echo "<tr>";
-                    echo "<td>{$medecin['civilite']}</td>";
+                    echo "<th scope=\"row\">{$medecin['civilite']}</th>";
                     echo "<td>{$medecin['nom']}</td>";
                     echo "<td>{$medecin['prenom']}</td>";
                     echo "</tr>";
                 }
-    ?>
-                </table>
-                <br><br>
-                <a href="ajout_medecin.php">Ajouter un medecin</a>
-    <?php
-            }    
+            }
         }
+                
     ?>
 
-    
+    </tbody>
+  </table>
+  <a href="ajout_medecin.php"><button>Ajouter un medecin</button></a>
+
+</div>
 </body>
 </html>
 
--- affichage_patient.php
+++ affichage_patient.php
@@ -3,16 +3,36 @@
 <head>
     <meta charset="utf-8" />
     <title>Les Patients</title>
-    <link href="style.css" rel="stylesheet">
+    <link href="style.scss" rel="stylesheet">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <?php
-        require('header.php');
-        require('bd_connexion.php');
+     require('header.php');
+     require('bd_connexion.php');
+     ?>
 
+    <!-- modifier le css des tableaux dans style.scss -->
+    <div class="container">
+        <table class="responsive-table">
+            <thead>
+            <tr>
+                <th>Civilité</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Adresse</th>
+                <th>Code Postal</th>
+                <th>Ville</th>
+                <th>Date de naissance</th>
+                <th>Lieu de naissance</th>
+                <th>N° sécurité sociale</th>
+            </tr>
+            </thead>
+            <tbody>
+
+    <?php
         // Préparation de la requête de recherche des patients
         $reqAffichage = $linkpdo->prepare('SELECT civilite, nom, prenom, adresse, ville, cp, date_naissance, lieu_naissance, num_secu_sociale FROM patient');
 
@@ -25,25 +45,10 @@
             if ($reqAffichage == false) {
                 echo "Erreur dans l'exécution de la requête d'affichage.";
             } else {
-                // Affichage des résultats dans un tableau
-    ?>
-                <table border="1">
-                    <tr>
-                        <th>Civilité</th>
-                        <th>Nom</th>
-                        <th>Prénom</th>
-                        <th>Adresse</th>
-                        <th>Code Postal</th>
-                        <th>Ville</th>
-                        <th>Date de naissance</th>
-                        <th>Lieu de naissance</th>
-                        <th>Numéro de sécurité sociale</th>
-                    </tr>
-    <?php
                 // Récupération des résultats et affichage dans le tableau
                 while ($patient = $reqAffichage->fetch(PDO::FETCH_ASSOC)) {
                     echo "<tr>";
-                    echo "<td>{$patient['civilite']}</td>";
+                    echo "<th scope=\"row\">{$patient['civilite']}</th>";
                     echo "<td>{$patient['nom']}</td>";
                     echo "<td>{$patient['prenom']}</td>";
                     echo "<td>{$patient['adresse']}</td>";
@@ -54,16 +59,14 @@
                     echo "<td>{$patient['num_secu_sociale']}</td>";
                     echo "</tr>";
                 }
-    ?>
-                </table>
-                <br><br>
-                <a href="ajout_patient.php">Ajouter un patient</a>
-    <?php
-            }    
-        }
-    ?>
+            }
+        }    ?>
+        
+        </tbody>
+    </table>
+    <a href="ajout_patient.php"><button>Ajouter un patient</button></a>
 
-    
+</div>
 </body>
 </html>
 
