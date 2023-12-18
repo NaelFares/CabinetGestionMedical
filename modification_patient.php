2b8e0e9ed2747c10fc39bc240f744bd6e20dd8ea
nesriiinee <nesrineben.317@gmail.com>
2023-12-18 17:09:07 +0100

    affiche pas

--- modification_patient.php
+++ modification_patient.php
@@ -1,87 +1,149 @@
+
 <!DOCTYPE HTML>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8" />
-        <title>Modification d'un patient</title>
-    </head>
-    <body>
-        <?php
+<head>
+    <meta charset="utf-8" />
+    <title>Modification d'un patient</title>
+    <link href="style.css" rel="stylesheet">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>  
+</head>
 
-            if (!empty($_GET['nom']) && !empty($_GET['prenom']) && !empty($_GET['adresse']) && !empty($_GET['ville']) && !empty($_GET['cp']) &&  !empty($_GET['date_naissance']) &&  !empty($_GET['lieu_naissance']) &&  !empty($_GET['num_secu_sociale'])) {
-            // Récupérez les valeurs des paramètres GET
-            $civilite = $_GET['civilite'];
-            $nom = $_GET['nom'];
-            $prenom = $_GET['prenom'];
-            $adresse = $_GET['adresse'];
-            $ville = $_GET['ville'];
-            $cp = $_GET['cp'];
-            $date_naissance = $_GET['date_naissance'];
-            $lieu_naissance = $_GET['lieu_naissance'];
-            $num_secu_sociale = $_GET[',num_secu_sociale'];
-            }
-            ?>
+<body>
+    <?php
+    require('bd_connexion.php');
+    require('header.php');
 
+    if (!empty($_GET['civilite']) && !empty($_GET['nom']) && !empty($_GET['prenom']) && !empty($_GET['adresse']) && !empty($_GET['cp']) && !empty($_GET['ville']) && !empty($_GET['date_naissance']) && !empty($_GET['lieu_naissance']) && !empty($_GET['num_secu_sociale'])) {
+        // Récupérez les valeurs des paramètres GET
+        $civilite = $_GET['civilite'];
+        $nom = $_GET['nom'];
+        $prenom = $_GET['prenom'];
+        $adresse = $_GET['adresse'];
+        $cp = $_GET['cp'];
+        $ville = $_GET['ville'];
+        $date_naissance = $_GET['date_naissance'];
+        $lieu_naissance = $_GET['lieu_naissance'];
+        $num_secu_sociale = $_GET['num_secu_sociale'];
+    }
+    ?>
 
-            <br>
-            <?php
-            // Vérifiez si le formulaire a été soumis
-            if (!(isset($_POST['valider']) && $_POST['valider'] == 'valider')) {
-                ?>
-                <form method="post" action="">
-                    Civilité : <input type="text" name="civiliteModif" placeholder="Entrer la civilite" value="<?php echo $civilite; ?>"><br><br>
-                    Nom : <input type="text" name="nomModif" placeholder="Entrer le nom" value="<?php echo $nom; ?>"><br><br>
-                    Prénom : <input type="text" name="prenomModif" placeholder="Entrez le prénom" value="<?php echo $prenom; ?>"><br><br>
-                    Adresse : <input type="text" name="adresseModif" placeholder="Entrer l'adresse" value="<?php echo $adresse; ?>" ><br><br>
-                    Ville : <input type="text" name= "villeModif" placeholder="Entrer la ville" value="<?php echo $ville; ?>"><br><br>
-                    Code postal : <input type="text" name="cpModif" placeholder="Entrer le code postal" value="<?php echo $cp; ?>"><br><br>
-                    Date de naissance : <input type="text" name= "date_naissanceModif" placeholder="Entrer la date de naissance" value="<?php echo $date_naissance; ?>"><br><br>
-                    Lieu de naissance : <input type="text" name= "lieu_naissanceModif" placeholder="Entrer le lieu de naissance" value="<?php echo $lieu_naissance; ?>"><br><br>
-                    Numéro de sécurité sociale : <input type="text" name= "num_secu_socialeModif" placeholder="Entrer le numéro de sécurtité sociale" value="<?php echo $num_secu_sociale; ?>"><br><br>
-
-                    <input type="submit" value="valider" name="valider">
-                    <input type="reset" value="vider" name="vider">
+    <div class="row justify-content-center">
+        <div class=" col-lg-7 col-md-8">
+            <div class="card p-9">
+                <div class="row justify-content-center">
+                    <div class="col-12">
+                        <h2 class="heading text-center">Modifier un patient</h2>
+                        <h4 class="errormessage text-center"></h4>
+                    </div>
+                </div>
+                <form class="form-card" method="post" action="modification_patient.php">
+                    <div class="row justify-content-center form-group">
+                        <div class="col-12 px-auto">
+                            <fieldset>
+                                <div class="custom-control custom-radio custom-control-inline"> 
+                                    <input id="customRadioInline1" type="radio" name="civilite" value="Mme" class="custom-control-input" checked="true"> 
+                                    <label for="customRadioInline1" class="custom-control-label label-radio">Madame</label> 
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline"> 
+                                    <input id="customRadioInline2" type="radio" name="civilite" value="M." class="custom-control-input"> 
+                                    <label for="customRadioInline2" class="custom-control-label label-radio">Monsieur</label> 
+                                </div>
+                            </fieldset>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="input-group"> <input type="text" name="nom" required value="<?php echo $nom; ?>"> <label>Nom</label> </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="input-group"> <input type="text" name="prenom" required value="<?php echo $prenom; ?>"> <label>Prénom</label> </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="input-group"> <input type="text" name="adresse" required value="<?php echo $adresse; ?>"> <label>Adresse</label> </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="input-group"> <input type="text" minlength="5" maxlength="5" name="cp" required value="<?php echo $cp; ?>"> <label>Code Postal</label> </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="input-group"> <input type="text" name="ville" required value="<?php echo $ville; ?>"> <label>Ville</label> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-4">
+                                    <div class="input-group"> <input type="date" name="date_naissance" required value="<?php echo $date_naissance; ?>"> <label>Date de naissance</label> </div>
+                                </div>
+                                <div class="col-8">
+                                    <div class="input-group"> <input type="text" name="lieu_naissance" required value="<?php echo $lieu_naissance; ?>"> <label>Lieu de naissance</label> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="input-group"> <input type="text" name="num_secu_sociale" minlength="13" maxlength="13" required value="<?php echo $num_secu_sociale; ?>"> <label>Numéro de sécurité sociale </label> </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="col-12">
+                                <input type="submit" name="modifier_patient" value="Valider les modifications" class="btn-modifier">
+                            </div>
+                        </div>
+                    </div>
                 </form>
-            <?php
+            </div>
+        </div>
+    </div>
+    <!--Fin du formulaire-->
+
+    <?php
+    if (isset($_POST['modifier_patient']) && $_POST['modifier_patient'] == 'Valider les modifications' ) {
+        // Préparation de la requête d'insertion
+        // La prochaine fois utiliser + de paramètres dans le where pour éviter de modifier les infos d'un homonyme 
+        $reqModification = $linkpdo->prepare('UPDATE patient SET civilite = :nouvelleCivilite, nom = :nouveauNom, prenom = :nouveauPrenom, adresse = :nouvelleAdresse, ville = :nouvelleVille, cp = :nouveauCp, date_naissance = :nouvelleDate_naissance, lieu_naissance = :nouveauLieu_naissance, num_secu_sociale = :nouveauNum_secu_sociale WHERE nom = :nom AND prenom = :prenom');
+
+        if ($reqModification === false) {
+            echo "
+
+Erreur de préparation de la requête.";
+        } else {
+            $reqModification->bindParam(':nouvelleCivilite', $_POST['civilite'], PDO::PARAM_STR);
+            $reqModification->bindParam(':nouveauNom', $_POST['nom'], PDO::PARAM_STR);
+            $reqModification->bindParam(':nouveauPrenom', $_POST['prenom'], PDO::PARAM_STR);
+            $reqModification->bindParam(':nouvelleAdresse', $_POST['adresse'], PDO::PARAM_STR);
+            $reqModification->bindParam(':nouvelleVille', $_POST['ville'], PDO::PARAM_STR);
+            $reqModification->bindParam(':nouveauCp', $_POST['cp'], PDO::PARAM_STR);
+            $reqModification->bindParam(':nouvelleDate_naissance', $_POST['date_naissance'], PDO::PARAM_STR);
+            $reqModification->bindParam(':nouveauLieu_naissance', $_POST['lieu_naissance'], PDO::PARAM_STR);
+            $reqModification->bindParam(':nouveauNum_secu_sociale', $_POST['num_secu_sociale'], PDO::PARAM_STR);
+
+            $reqModification->bindParam(':nom', $nom, PDO::PARAM_STR);
+            $reqModification->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+
+            // Exécution de la requête
+            if($reqModification->execute() === false) {
+                echo "Erreur dans l'exécution de la requête de modification.";
+            } else {
+                // Afficher un message de succès
+                echo "Le patient a été modifié avec succès !";
             }
-            
-           require('bd_connexion.php');
+        }
+    }
+    ?>
 
-            // Cette ligne jsp
-            if (isset($_POST['valider']) && $_POST['valider'] == 'valider' ) {
-
-                // Préparation de la requête d'insertion
-                // La prochaine fois utiliser + de parametres dans le where pour éviter de modifier les infos d'un homonyme 
-                $reqModification = $linkpdo->prepare('UPDATE patient SET civilite = :nouvelleCivilite, nom = :nouveauNom, prenom = :nouveauPrenom, adresse = :nouvelleAdresse, ville = :nouvelleVille, cp = :nouveauCp, date_naissance = :nouvelleDate_naissance, lieu_naissance = :nouveauLieu_naissance, num_secu_sociale = :nouveauNum_secu_sociale WHERE nom = :nom AND prenom = :prenom');
-
-                if ($reqModification === false) {
-                    echo "Erreur de préparation de la requête.";
-                } else {
-                    $reqModification->bindParam(':nouvelleCivilite', $_POST['civiliteModif'], PDO::PARAM_STR);
-                    $reqModification->bindParam(':nouveauNom', $_POST['nomModif'], PDO::PARAM_STR);
-                    $reqModification->bindParam(':nouveauPrenom', $_POST['prenomModif'], PDO::PARAM_STR);
-                    $reqModification->bindParam(':nouvelleAdresse', $_POST['adresseModif'], PDO::PARAM_STR);
-                    $reqModification->bindParam(':nouvelleVille', $_POST['villeModif'], PDO::PARAM_STR);
-                    $reqModification->bindParam(':nouveauCp', $_POST['cpModif'], PDO::PARAM_STR);
-                    $reqModification->bindParam(':nouvelleDate_naissance', $_POST['date_naissanceModif'], PDO::PARAM_STR);
-                    $reqModification->bindParam(':nouveauLieu_naissance', $_POST['lieu_naissanceModif'], PDO::PARAM_STR);
-                    $reqModification->bindParam(':nouveauNum_secu_sociale', $_POST['num_secu_socialeModif'], PDO::PARAM_STR);
-
-                    $reqModification->bindParam(':nom', $nom, PDO::PARAM_STR);
-                    $reqModification->bindParam(':prenom', $prenom, PDO::PARAM_STR);
-
-                    // Exécution de la requête
-                    $reqModification->execute();
-
-                    if($reqModification == false) {
-                        echo "Erreur dans l'exécution de la requête de modification.";
-                    } else {
-
-                        // Afficher un message de succès
-                        echo "Le patient a été modifié avec succès !";
-                    }
-                }
-            }
-        ?>
-
-    </body>
+</body>
 </html>
