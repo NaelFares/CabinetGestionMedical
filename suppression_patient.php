2a19e56cd730a5b445b710b8b00358d2f213e0f3
ValentaneVacquie28 <150685341+ValentaneVacquie28@users.noreply.github.com>
2023-12-22 09:29:47 +0100

    Valou

--- suppression_patient.php
+++ suppression_patient.php
@@ -3,6 +3,10 @@
     <head>
         <meta charset="utf-8" />
         <title>Suppresion d'un patient</title>
+        <link href="style.scss" rel="stylesheet">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     </head>
     <body>
 
@@ -23,49 +27,67 @@
         ?>
 
         <br>
-        <h1> Voulez-vous supprimer le patient ? </h1>
-        <table border="1">
-            <tr>
-                <th>Civilite</th>
-                <th>Nom</th>
-                <th>Prénom</th>
-                <th>Adresse</th>
-                <th>Ville</th>
-                <th>Code Postal</th>
-                <th>Date de naissance</th>
-                <th>Lieu de naissance</th>
-                <th>Numéro de sécurité sociale</th>
+        <h1> Supprimer un patient </h1>
+        <div class="container">
+            <table class="responsive-table">
+                <thead>
+                    <tr>
+                        <th>Civilite</th>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Adresse</th>
+                        <th>Ville</th>
+                        <th>Code Postal</th>
+                        <th>Date de naissance</th>
+                        <th>Lieu de naissance</th>
+                        <th>Numéro de sécurité sociale</th>
+                    </tr>
+                </thead>
 
+                <tbody>
+                    <?php
+                    $reqAffichage = $linkpdo->prepare('SELECT civilite, nom, prenom, adresse, ville, cp, date_naissance, lieu_naissance, num_secu_sociale FROM patient');
 
-            </tr>
-            <tr>
-                <td><?php echo $civilite; ?></td>
-                <td><?php echo $nom; ?></td>
-                <td><?php echo $prenom; ?></td>
-                <td><?php echo $adresse; ?></td>
-                <td><?php echo $ville; ?></td>
-                <td><?php echo $cp; ?></td>
-                <td><?php echo $date_naissance; ?></td>
-                <td><?php echo $lieu_naissance; ?></td>
-                <td><?php echo $num_secu_sociale; ?></td>
+                    if ($reqAffichage == false) {
+                        echo "Erreur dans la préparation de la requête de recherche.";
+                    } else {
+                        $reqAffichage->execute();
 
-            </tr>
-        </table>
+                        if ($reqAffichage == false) {
+                            echo "Erreur dans l'exécution de la requête d'affichage.";
+                        } else {
+                            while ($patient = $reqAffichage->fetch(PDO::FETCH_ASSOC)) {
+                                echo "<tr onclick=\"selectionnerLigne(this)\">";
+                                echo "<th>{$patient['civilite']}</th>";
+                                echo "<td>{$patient['nom']}</td>";
+                                echo "<td>{$patient['prenom']}</td>";
+                                echo "<td>{$patient['adresse']}</td>";
+                                echo "<td>{$patient['cp']}</td>";
+                                echo "<td>{$patient['ville']}</td>";
+                                echo "<td>{$patient['date_naissance']}</td>";
+                                echo "<td>{$patient['lieu_naissance']}</td>";
+                                echo "<td>{$patient['num_secu_sociale']}</td>";
+                                echo "</tr>";
+                            }
+                        }
+                    }
+                    ?>
+                </tbody>
+            </table>
 
-        <br><br>
-        <form method="post">
-            <input type="submit" name="confirmer" value="Confirmer la suppression">
-            <input type="submit" name="annuler" value="Annuler">
-        </form>
+            <a href="affichage_consultation.php" id="bouttonSupression" name="bouttonSupression"><button>Confirmer la suppression</button></a>
+            <a href="affichage_patient.php" id="bouttonAnnuler" name="bouttonAnnuler"><button>Annuler</button></a>
+            
+        </div>
 
 
         <?php
-       require('bd_connexion.php');
+        require('bd_connexion.php');
 
         // Si le bouton confirmer à été pressé
-        if (isset($_POST['confirmer'])) {
+        if (isset($_POST['bouttonSupression'])) {
 
-            // Préparation de la requête d'insertion
+            // Préparation de la requête de supression
             // La prochaine fois utiliser + de parametres dans le where pour éviter de modifier les infos d'un homonyme 
             $reqSuppression = $linkpdo->prepare('DELETE FROM patient WHERE civilite = :nouvelleCivilite, nom = :nouveauNom, prenom = :nouveauPrenom, adresse = :nouvelleAdresse, ville = :nouvelleVille, cp = :nouveauCp, date_naissance = :nouvelleDate_naissance, lieu_naissance = :nouveauLieu_naissance, num_secu_sociale = :nouveauNum_secu_sociale');
 
@@ -89,13 +111,14 @@
                 if($reqSuppression == false) {
                     echo "Erreur dans l'exécution de la requête de suppression.";
                 } else {
-                    // Redirigez l'utilisateur vers la page recherche.php
+                    // Redirigez l'utilisateur vers la page affichage_patient.php
                     header("Location: affichage_patient.php");
                     exit;
                 }
             }
         }
         ?>
+
 
     </body>
 </html>
