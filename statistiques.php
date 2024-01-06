0bbe1336210da9848677a5fb5a0fcbad523e0ac3
nesriiinee <nesrineben.317@gmail.com>
2024-01-06 23:31:04 +0100

    creation page statistiques
    elle est vide encore

---
+++ statistiques.php
@@ -0,0 +1,25 @@
+<?php
+require('module/verificationUtilisateur.php');
+?>
+
+<!DOCTYPE HTML>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <title>Les Statistiques</title>
+    <link href="style/tableau.css" rel="stylesheet">
+    <link href="style/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php
+     require('module/header.php');
+     require('module/bd_connexion.php');
+     ?>
+
+    <!--Espace vide pour permettre de placer le header en haut de page-->
+    <div class="vide-haut-page"> </div>
+
+    
+</body>
+</html>
