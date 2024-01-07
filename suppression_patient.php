8999df9f7c5260567db7eae0df988844b7f24c23
NaelFares <frs.nael@gmail.com>
2024-01-07 01:36:24 +0100

    style page suppression
    - penser à supprimer une consultation avant de supp le patient

--- suppression_patient.php
+++ suppression_patient.php
@@ -7,11 +7,11 @@ require('module/verificationUtilisateur.php');
 <html lang="fr">
 <head>
     <meta charset="utf-8" />
-    <title>Modification d'un patient</title>
+    <title>Suppression d'un patient</title>
     <link href="style/style.css" rel="stylesheet">
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>  
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
@@ -39,16 +39,6 @@ require('module/verificationUtilisateur.php');
         $msgErreur = ""; // Déclaration de la variable de message d'erreur
 
         if (isset($_POST['supprimer_patient'])) {
-            $civilite = $_POST['civilite'];
-            $nom = $_POST['nom'];
-            $prenom = $_POST['prenom'];
-            $adresse = $_POST['adresse'];
-            $cp = $_POST['cp'];
-            $ville = $_POST['ville'];
-            $date_naissance = $_POST['date_naissance'];
-            $lieu_naissance = $_POST['lieu_naissance'];
-            $num_secu_sociale = $_POST['num_secu_sociale'];
-            $idM = $_POST['idM'];
             // Préparation de la requête de suppression
             // La prochaine fois utiliser + de paramètres dans le where pour éviter de supprimer les infos d'un homonyme  
             $reqSuppression = $linkpdo->prepare('DELETE FROM patient WHERE civilite = :civilite AND nom = :nom AND prenom = :prenom AND adresse = :adresse AND cp = :cp AND ville = :ville AND date_naissance = :date_naissance AND lieu_naissance = :lieu_naissance AND num_secu_sociale = :num_secu_sociale');
@@ -71,158 +61,47 @@ require('module/verificationUtilisateur.php');
                 $reqSuppression->execute();
 
                 if ($reqSuppression === false) {
-                    echo "Erreur dans l'exécution de la requête de suppression : ";
+                    $msgErreur = "Erreur dans l'exécution de la requête de suppression : ";
                 } else {
                     // Afficher un message de succès
-                    $msgErreur = "Le medecin a été supprimé avec succès !";
-
-                    // Vider les valeurs dans les champs de saisie pour éviter les erreurs de récupération de champs vides par $_POST
-                    $civilite = null;
-                    $nom = null;
-                    $prenom = null;
-                    $adresse = null;
-                    $cp = null;
-                    $ville = null;
-                    $date_naissance = null;
-                    $lieu_naissance = null;
-                    $num_secu_sociale = null;
-                    $idM = null;
+                    $msgErreur = "Le patient a été supprimé avec succès !";
+                   
                 }
             }
         }
         ?>
 
-
-    
-
-    <!--Espace vide pour permettre de placer le header en haut de page-->
-    <div class="vide-haut-page"> </div>
-
-    <div class="row justify-content-center">
-        <div class=" col-lg-7 col-md-8">
-            <div class="card p-9">
-                <div class="row justify-content-center">
-                    <div class="col-12">
-                        <h2 class="heading text-center">Modifier un patient</h2>
-                    </div>
-                    <div class="errormessage text-center">
-                        <p><?php echo $msgErreur; ?></p>
-                    </div>
-                </div>
-                <!--Ne pas spécifier de action pour que le traitement php se fasse sur la page actuelle-->
-                <form class="form-card" method="post" action="">
-                    <div class="row justify-content-center form-group">
-                        <div class="col-12 px-auto">
-                            <fieldset>
-                                <div class="custom-control custom-radio custom-control-inline"> 
-                                    <input id="customRadioInline1" type="radio" name="civilite" value="Mme" class="custom-control-input" 
-                                    <?php echo ($civilite == 'Mme') ? 'checked' : ''; ?> readonly> <!--Vérifie et coche la case correspondant à la civilité-->
-                                    <label for="customRadioInline1" class="custom-control-label label-radio">Madame</label> 
-                                </div>
-                                <div class="custom-control custom-radio custom-control-inline"> 
-                                    <input id="customRadioInline2" type="radio" name="civilite" value="M." class="custom-control-input" 
-                                    <?php echo ($civilite == 'M.') ? 'checked' : ''; ?> readonly> <!--Vérifie et coche la case correspondant à la civilité-->
-                                    <label for="customRadioInline2" class="custom-control-label label-radio">Monsieur</label> 
-                                </div>
-                            </fieldset>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <div class="input-group"> <input type="text" name="nom" required value="<?php echo $nom; ?>" readonly> <label>Nom</label> </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <div class="input-group"> <input type="text" name="prenom" required value="<?php echo $prenom; ?>" readonly> <label>Prénom</label> </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <div class="input-group"> <input type="text" name="adresse" required value="<?php echo $adresse; ?>" readonly> <label>Adresse</label> </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="input-group"> <input type="text" minlength="5" maxlength="5" name="cp" required value="<?php echo $cp; ?>" readonly> <label>Code Postal</label> </div>
-                                </div>
-                                <div class="col-6">
-                                    <div class="input-group"> <input type="text" name="ville" required value="<?php echo $ville; ?>" readonly> <label>Ville</label> </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="col-4">
-                                      <!--Définit la date max comme la date actuelle, permet d'éviter de mettre une date de naissance antérieure-->
-                                    <div class="input-group"> <input type="date" name="date_naissance" required value="<?php echo $date_naissance; ?>" max="<?php echo date('Y-m-d'); ?>" readonly> <label>Date de naissance</label> </div>
-                                </div>
-                                <div class="col-8">
-                                    <div class="input-group"> <input type="text" name="lieu_naissance" required value="<?php echo $lieu_naissance; ?>" readonly> <label>Lieu de naissance</label> </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                        <div class="col-12">
-                            <div class="input-group"> <input type="text" name="num_secu_sociale" minlength="13" maxlength="13" required value="<?php echo $num_secu_sociale; ?>" readonly> <label>Numéro de sécurité sociale </label> </div>
-                        </div>
-                    </div>
-                    <div class="row justify-content-center">
-                                <div class="col-12">
-                                <label> Choisissez un medecin référent (facultatif) </label>
-                                    <div class="input-group">
-                                        <select name="idM"> <!--required-->
-
-                                             <?php
-                                            //requête pour afficher le médecin référent du patient qu'on modifie à partir de l'Id du médecin récupéré
-                                            $reqMedecinRefActuel = $linkpdo->prepare('SELECT civilite,nom, prenom FROM medecin WHERE idM = :idM');
-                                            $reqMedecinRefActuel->bindParam(':idM', $idM, PDO::PARAM_INT);
-                                            $reqMedecinRefActuel->execute();
-                                            //On prend l'enregistrement du medecin actuel
-                                            $medecinActuel = $reqMedecinRefActuel->fetch(PDO::FETCH_ASSOC);
-                                            //On associe les valeurs à des variables pour les affichées dans l'option
-                                            $civiliteMedecinActuel = $medecinActuel['civilite'];
-                                            $nomMedecinActuel = $medecinActuel['nom'];
-                                            $prenomMedecinActuel = $medecinActuel['prenom'];
-                                            echo "<option value=\"$idM\">$civiliteMedecinActuel $nomMedecinActuel $prenomMedecinActuel</option>";
-                                            ?>
-
-                                            <option value="Aucun">Aucun</option>
-
-                                            <?php
-                                            //requête pour afficher la liste des medecins pour le choix d'un medecin référent
-                                            $reqMedecins = $linkpdo->prepare('SELECT idM, civilite,nom, prenom FROM medecin');
-                                            $reqMedecins->execute();
-                                            while ($medecin = $reqMedecins->fetch(PDO::FETCH_ASSOC)) {
-                                                $idMedecin = $medecin['idM'];
-                                                $civiliteMedecin = $medecin['civilite'];
-                                                $nomMedecin = $medecin['nom'];
-                                                $prenomMedecin = $medecin['prenom'];
-                                                echo "<option value=\"$idMedecin\">$civiliteMedecin $nomMedecin $prenomMedecin</option>";}
-                                            ?>
-                                            
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                    <div class="row justify-content-center">
-                        <div class="col-12">
+    <div class="centrer-milieu-page">
+        <div class="row justify-content-center">
+            <div class=" col-lg-7 col-md-8">
+                <div class="card p-9">
+                    <form class="form-card" method="post" action="">
+                        <div class="row justify-content-center">
                             <div class="col-12">
-                                <input type="submit" name="supprimer_patient" value="Supprimer le patient" class="btn">
-                                <input type="button" onclick="window.location.href='affichage_patient.php';" value="Annuler" class="">
+                                <h3 class="titre-suppression">Voulez-vous vraiment supprimer ce patient ?</h3>
+                            </div>
+                            <div class="errormessage text-center">
+                                <p><?php echo $msgErreur; ?></p>
+                            </div>
+                            <div class="informations-patient">
+                                <p class="informations-nom-patient"><?php echo $civilite . " " . $nom . " " . $prenom?></p>
+                                <p> Date de naissance : <?php echo $date_naissance?></p> 
+                                <p> Num sécurité sociale : <?php echo $num_secu_sociale?></p> 
                             </div>
                         </div>
-                    </div>
-                </form>
+                        <div class="row justify-content-center">
+                            <div class="col-12">
+                                <div class="boutons-suppression">
+                                    <a href="affichage_patient.php" class="btn-supp-annuler">Retour à l'accueil</a>
+                                    <div><input class="input-supp-valider" type="submit" name="supprimer_patient" value="Supprimer le patient"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-    <!--Fin du formulaire-->
+    
 </body>
 </html>
-
